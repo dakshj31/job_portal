49d065046bcb1d1ef7f05e0927aec9dcecf0147d
dakshj31 <dakshjoshi1355@gmail.com>
2024-08-23 00:22:20 +0530

    10

--- public/job-detail.php
+++ public/job-detail.php
@@ -3,7 +3,36 @@ include "../resources/templates/front/header.php";
 
 /***  job details query  ***/
 if (isset($_GET['id'])) {
-    
+
+    $jobs_data_query = query("SELECT * FROM jobs WHERE id =" . escape_string($_GET['id']));
+    confirm($jobs_data_query);
+
+    while ($row = fetch_array($jobs_data_query)) {
+        
+        $title = escape_string($row['title']);
+        $description = escape_string($row['description']);
+        $vacancy = escape_string($row['vacancy']);
+        $nature = escape_string($row['nature']);
+        $knowledge = escape_string($row['knowledge']);
+        $skills = escape_string($row['skills']);
+        $education = escape_string($row['education']);
+        $experience = escape_string($row['experience']);
+        $salary = escape_string($row['salary']);
+        $location = escape_string($row['location']);
+        $posted_on = escape_string($row['created at']);
+
+        /*** company data ***/
+        $company_name = escape_string($row['company_name']);
+        $company_data_query = query("SELECT * FROM user WHERE username= '{$company_name}' ");
+        confirm($company_data_query);
+
+        while ($row = fetch_array($company_data_query)) {
+
+            $company_id = $row['user_id'];
+            // $company_description = $row['description'];
+            $company_email = $row['email'];
+        }
+    }
 }
 
  ?>
@@ -32,34 +61,23 @@ if (isset($_GET['id'])) {
                         <div class="d-flex align-items-center mb-5">
                             <img class="flex-shrink-0 img-fluid border rounded" src="img/com-logo-2.jpg" alt="" style="width: 80px; height: 80px;">
                             <div class="text-start ps-4">
-                                <h3 class="mb-3">Marketing Manager</h3>
-                                <span class="text-truncate me-3"><i class="fa fa-map-marker-alt text-primary me-2"></i>New York, USA</span>
-                                <span class="text-truncate me-3"><i class="far fa-clock text-primary me-2"></i>Full Time</span>
-                                <span class="text-truncate me-0"><i class="far fa-money-bill-alt text-primary me-2"></i>$123 - $456</span>
+                                <h3 class="mb-3"><?php echo $title; ?></h3>
+                                <span class="text-truncate me-3"><i class="fa fa-map-marker-alt text-primary me-2"></i><?php echo $location; ?></span>
+                                <span class="text-truncate me-3"><i class="far fa-clock text-primary me-2"></i><?php echo $nature; ?></span>
+                                <span class="text-truncate me-0"><i class="far fa-money-bill-alt text-primary me-2"></i>&#8377;<?php echo $salary; ?></span>
                             </div>
                         </div>
 
                         <div class="mb-5">
                             <h4 class="mb-3">Job description</h4>
-                            <p>Dolor justo tempor duo ipsum accusam rebum gubergren erat. Elitr stet dolor vero clita labore gubergren. Kasd sed ipsum elitr clita rebum ut sea diam tempor. Sadipscing nonumy vero labore invidunt dolor sed, eirmod dolore amet aliquyam consetetur lorem, amet elitr clita et sed consetetur dolore accusam. Vero kasd nonumy justo rebum stet. Ipsum amet sed lorem sea magna. Rebum vero dolores dolores elitr vero dolores magna, stet sea sadipscing stet et. Est voluptua et sanctus at sanctus erat vero sed sed, amet duo no diam clita rebum duo, accusam tempor takimata clita stet nonumy rebum est invidunt stet, dolor.</p>
-                            <h4 class="mb-3">Responsibility</h4>
-                            <p>Magna et elitr diam sed lorem. Diam diam stet erat no est est. Accusam sed lorem stet voluptua sit sit at stet consetetur, takimata at diam kasd gubergren elitr dolor</p>
-                            <ul class="list-unstyled">
-                                <li><i class="fa fa-angle-right text-primary me-2"></i>Dolor justo tempor duo ipsum accusam</li>
-                                <li><i class="fa fa-angle-right text-primary me-2"></i>Elitr stet dolor vero clita labore gubergren</li>
-                                <li><i class="fa fa-angle-right text-primary me-2"></i>Rebum vero dolores dolores elitr</li>
-                                <li><i class="fa fa-angle-right text-primary me-2"></i>Est voluptua et sanctus at sanctus erat</li>
-                                <li><i class="fa fa-angle-right text-primary me-2"></i>Diam diam stet erat no est est</li>
-                            </ul>
-                            <h4 class="mb-3">Qualifications</h4>
-                            <p>Magna et elitr diam sed lorem. Diam diam stet erat no est est. Accusam sed lorem stet voluptua sit sit at stet consetetur, takimata at diam kasd gubergren elitr dolor</p>
-                            <ul class="list-unstyled">
-                                <li><i class="fa fa-angle-right text-primary me-2"></i>Dolor justo tempor duo ipsum accusam</li>
-                                <li><i class="fa fa-angle-right text-primary me-2"></i>Elitr stet dolor vero clita labore gubergren</li>
-                                <li><i class="fa fa-angle-right text-primary me-2"></i>Rebum vero dolores dolores elitr</li>
-                                <li><i class="fa fa-angle-right text-primary me-2"></i>Est voluptua et sanctus at sanctus erat</li>
-                                <li><i class="fa fa-angle-right text-primary me-2"></i>Diam diam stet erat no est est</li>
-                            </ul>
+                            <p><li><?php echo $description; ?></li></p>
+                            <h4 class="mb-3">Knowledge</h4>
+                            <p><li><?php echo $knowledge; ?></li></p>
+                            
+                            <h4 class="mb-3">Education & Experience</h4>
+                            <li><?php echo $education; ?></li>
+                            <li><?php echo $experience; ?></li>
+                           
                         </div>
         
                         <div class="">
@@ -92,16 +110,17 @@ if (isset($_GET['id'])) {
                     <div class="col-lg-4">
                         <div class="bg-light rounded p-5 mb-4 wow slideInUp" data-wow-delay="0.1s">
                             <h4 class="mb-4">Job Summery</h4>
-                            <p><i class="fa fa-angle-right text-primary me-2"></i>Published On: 01 Jan, 2045</p>
-                            <p><i class="fa fa-angle-right text-primary me-2"></i>Vacancy: 123 Position</p>
-                            <p><i class="fa fa-angle-right text-primary me-2"></i>Job Nature: Full Time</p>
-                            <p><i class="fa fa-angle-right text-primary me-2"></i>Salary: $123 - $456</p>
-                            <p><i class="fa fa-angle-right text-primary me-2"></i>Location: New York, USA</p>
-                            <p class="m-0"><i class="fa fa-angle-right text-primary me-2"></i>Date Line: 01 Jan, 2045</p>
+                            <p><i class="fa fa-angle-right text-primary me-2"></i>Published On: <?php echo $posted_on; ?></p>
+                            <p><i class="fa fa-angle-right text-primary me-2"></i>Vacancy: <?php echo $vacancy; ?> Position</p>
+                            <p><i class="fa fa-angle-right text-primary me-2"></i>Job Nature: <?php echo $nature; ?></p>
+                            <p><i class="fa fa-angle-right text-primary me-2"></i>Salary: &#8377;<?php echo $salary; ?></p>
+                            <p><i class="fa fa-angle-right text-primary me-2"></i>Location: <?php echo $location; ?></p>
                         </div>
                         <div class="bg-light rounded p-5 wow slideInUp" data-wow-delay="0.1s">
                             <h4 class="mb-4">Company Detail</h4>
-                            <p class="m-0">Ipsum dolor ipsum accusam stet et et diam dolores, sed rebum sadipscing elitr vero dolores. Lorem dolore elitr justo et no gubergren sadipscing, ipsum et takimata aliquyam et rebum est ipsum lorem diam. Et lorem magna eirmod est et et sanctus et, kasd clita labore.</p>
+                            <p class="m-0"> Name : <?php echo $company_name; ?></p>
+                            <p class="m-0"> Email : <?php echo $company_email; ?></p>
+                            <p class="m-0"><?php ?></p>
                         </div>
                     </div>
                 </div>
--- resources/templates/front/top-nav.php
+++ resources/templates/front/top-nav.php
@@ -14,7 +14,7 @@
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Jobs</a>
                         <div class="dropdown-menu rounded-0 m-0">
                             <a href="job-list.php" class="dropdown-item">Job List</a>
-                            <a href="job-detail.php" class="dropdown-item">Job Detail</a>
+                            <!-- <a href="job-detail.php" class="dropdown-item">Job Detail</a> -->
                         </div>
                     </div>
                     <div class="nav-item dropdown">
