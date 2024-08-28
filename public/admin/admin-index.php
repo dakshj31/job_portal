330b090f7c369f6a00026750233ff6f5d7d56f07
dakshj31 <dakshjoshi1355@gmail.com>
2024-08-28 21:46:17 +0530

    16

--- public/admin/admin-index.php
+++ public/admin/admin-index.php
@@ -6,6 +6,64 @@ if(!isset($_SESSION['username']) ) {
     redirect("index.php");
 } ?>
 
+<!-- Hero Area Start-->
+<div class="slider-area ">
+    <div class="single-slider section-overly slider-height2 d-flex align-items-center" data-background="../assets/img/hero/about.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="hero-cap text-center">
+                        <h2><i class="fas fa-user" style="font-size:100px;"></i> Company Data</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<!-- Hero Area End -->
+
+<div class="container">
+    <br>
+
+    <!-- Right Content -->
+    <div class="col-xl-12 col-lg-2">
+        <div class="post-details3  mb-10">
+            <!-- Small Section Tittle -->
+            <div class="small-section-tittle">
+                <h4 class="text-center"><i class="far fa-list-alt" style="font-size: 40px;"></i> ALL DATA...!!</h4>
+            </div>
+            <hr>
+            <ul>
+                <li><i class="fas fa-briefcase" style="font-size: 80px;"></i>
+                    TOTAL JOBS POSTED ===============================================> <?php //count_jobs(); ?> Jobs !<div class="text-center">
+                        <div class="header-btn d-none f-center d-lg-block">
+                            <p><a href="../admin_jobs.php" class="btn border-btn head-btn1">View All Jobs !</a></p>
+                        </div>
+                    </div>
+                </li>
+                <hr>
+                <li><i class="fas fa-building" style="font-size: 80px;"></i>
+                    TOTAL COMPANIES REGISTERED ======================================> <?php //count_companies(); ?> Companies !<div class="text-center">
+                        <div class="header-btn d-none f-center d-lg-block">
+                            <p><a href="../admin_companies.php" class="btn border-btn head-btn1">View All Companies !</a></p>
+                        </div>
+                    </div>
+                </li>
+                <hr>
+                <li><i class="fas fa-address-card" style="font-size: 80px;"></i>
+                    TOTAL CANDIDATES REGISTERED =====================================> <?php //count_candidates(); ?> Candidates !<div class="text-center">
+                        <div class="header-btn d-none f-center d-lg-block">
+                            <p><a href="../admin_candidates.php" class="btn border-btn head-btn1">View All Candidates !</a></p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</div>
+<br>
 
 
 <?php include "includes/footer.php";?>
